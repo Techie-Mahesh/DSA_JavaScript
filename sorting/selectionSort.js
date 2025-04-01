d5efb73aefbd44012a9d7611bf8b2622a71ac315
Techie-Mahesh <maheshTech1205@gmail.com>
2025-04-01 23:25:13 +0530

    selection sort

---
+++ sorting/selectionSort.js
@@ -0,0 +1,20 @@
+const arr = [5, 9, 10, 48, 1, 67];
+
+const selectionSort = arr => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  console.log("`arr` after sorting: ", arr);
+};
+
+selectionSort(arr);

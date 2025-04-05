bd3546c928caeb38b225b2ff2437e8d59d7b355d
Techie-Mahesh <maheshTech1205@gmail.com>
2025-04-05 20:57:14 +0530

    CountPairs of AG optimised

--- Array/countPairAG.js
+++ Array/countPairAG.js
@@ -20,7 +20,7 @@ const countPairs = arr => {
 console.log(countPairs(["h", "b", "g", "a", "a", "g", "a", "g", "h", "g"])); // 8
 
 // ---------------------------------------------------------------------------
-// Optimized approach ( T.C O(n) and S.C O(1) )
+// Optimized approach ( T.C O(n) and S.C O(n) )
 // The optimized approach is to use a single loop to count the number of "a"s and "g"s.
 //  create a prefix sum array to store the count of "g"s up to each index.
 //   Example: for the array ["h","b","g","a","a","g","a","g","h","g"],
@@ -50,4 +50,48 @@ const countPairsOptimized = arr => {
 
 console.log(
   countPairsOptimized(["h", "b", "g", "a", "a", "g", "a", "g", "h", "g"])
-); // 5
+); // 8
+
+// ---------------------------------------------------------------------------
+// Optimized approach ( T.C O(n) and S.C O(1) )
+// The optimized approach is to use a single loop to count the number of "a"s and "g"s.
+
+const countPairsOptimized2 = arr => {
+  let count_G = 0;
+  let count_Ag = 0;
+ for(let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === "g") {
+      count_G++;
+    } else if (arr[i] === "a") {
+      count_Ag += count_G;
+    }
+  }
+  return count_Ag;
+};
+
+console.log(
+  countPairsOptimized2(["h", "b", "g", "a", "a", "g", "a", "g", "h", "g"])
+); // 8
+
+
+const countPairsOptimized3 = arr => {
+  let countG = 0;
+  let countAg = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "g") {
+      countG = countG + 1;
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "g") {
+      countG = countG - 1;
+    }
+    if (arr[i] === "a") {
+      countAg += countG;
+    }
+  }
+  return countAg;
+};
+console.log(
+  countPairsOptimized3(["h", "b", "g", "a", "a", "g", "a", "g", "h", "g"])
+); // 8

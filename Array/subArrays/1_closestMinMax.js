13b8defb5a047c92cb5503954aef4edc7715b172
Techie-Mahesh <maheshTech1205@gmail.com>
2025-04-15 20:51:45 +0530

    closest min Max

--- Array/subArrays/1_closestMinMax.js
+++ Array/subArrays/1_closestMinMax.js
@@ -1,6 +1,5 @@
-// Problem: Given an array of integers, find the length of the smallest subarray that contains both the minimum and maximum elements of the array. 
+// Problem: Given an array of integers, find the length of the smallest subarray that contains both the minimum and maximum elements of the array.
 // If the array is empty or has only one element, return 0.
-
 
 // Approach: Use two nested loops to find the minimum and maximum elements, and then find the length of the smallest subarray that contains both elements.
 // // Time Complexity: O(n^2) and space complexity: O(1)
@@ -38,8 +37,39 @@ const closestMinMax = arr => {
 };
 
 console.log(closestMinMax([1, 2, 3, 4, 5])); // 5
-console.log(closestMinMax( [2, 6, 1, 6, 9])); // 3
+console.log(closestMinMax([2, 6, 1, 6, 9])); // 3
 
 console.log("--------------------------------------------------");
 
 // optimized approach: O(n) and space complexity: O(1)
+
+const closestMinMax2 = arr => {
+  if (arr.length === 0) return 0;
+  if (arr.length === 1) return 1;
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  if (min === max) return 1;
+
+  let res = arr.length;
+  let minIndex = -1;
+  let maxIndex = -1;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === min) {
+      minIndex = i;
+      if (maxIndex !== -1) {
+        res = Math.min(res, minIndex - maxIndex + 1);
+      }
+    }
+    if (arr[i] === max) {
+      maxIndex = i;
+      if (minIndex !== -1) {
+        res = Math.min(res, maxIndex - minIndex + 1);
+      }
+    }
+  }
+  return res;
+};
+
+console.log(closestMinMax2([1, 2, 3, 4, 5])); // 5
+console.log(closestMinMax2([2, 6, 1, 6, 9])); // 3

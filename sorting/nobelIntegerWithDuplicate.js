b1422ec2f17be51094ee86d37af6c2017d28e661
Techie-Mahesh <maheshTech1205@gmail.com>
2025-04-27 18:51:12 +0530

    nobel Integer count for duplicate numbers

---
+++ sorting/nobelIntegerWithDuplicate.js
@@ -0,0 +1,30 @@
+// Noble Integer
+// A noble integer is an integer x such that the number of integers Lesser than x is equal to x.
+// For example, 3 is a noble integer because there are 3 integers Lesser than 3 in the array [1, -5, 3, 5, -10, 4].
+//  original Array: [-10, 0, 2, 5, 2, 4, 4, 5]
+//  sorted Array:   [-10, 0, 2, 2, 4, 4, 5, 5]
+//  prefix Array:   [  0, 1, 2, 2, 4, 4, 6, 6]
+//  index:          [  0, 1, 2, 3, 4, 5, 6, 7]
+
+const nobelIntegerWithDuplicate = arr => {
+  let sortedArray = arr.sort();
+  let prefixArray = new Array(arr.length);
+  let count = 0;
+  prefixArray[0] = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i - 1]) {
+      prefixArray[i] = prefixArray[i - 1];
+    } else {
+      prefixArray[i] = i;
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === prefixArray[i]) {
+      count++;
+    }
+  }
+  return count;
+};
+console.log(nobelIntegerWithDuplicate([1, -5, 3, 5, -10, 4])); // 5
+console.log(nobelIntegerWithDuplicate([1, 2, 0, 4, 5])); // 0
+console.log(nobelIntegerWithDuplicate([-10, 0, 2, 5, 2, 4, 4, 5])); // 4

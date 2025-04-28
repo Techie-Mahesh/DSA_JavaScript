ac1fa193c6d74170f18194f8c0fedcc86330c50f
Techie-Mahesh <maheshTech1205@gmail.com>
2025-04-28 17:16:37 +0530

    Max sum of k

--- Array/MaxSumOfK.js
+++ Array/MaxSumOfK.js
@@ -1,3 +1,5 @@
+// Description: Find the maximum sum of k consecutive elements in an array.
+// Example: Given arr = [1, 2, 8, 9, 3, 20, 4, 5, 6, 7, 8, 9] and k = 3, the maximum sum is 32 (20 + 9 + 3).
 const maxSumOfNNumbers = (arr, k) => {
   let currentSum = 0;
   let maxSum = 0;
@@ -8,14 +10,10 @@ const maxSumOfNNumbers = (arr, k) => {
 
   for (let i = k; i < arr.length; i++) {
     currentSum = currentSum - arr[i - k] + arr[i];
-    console.log(maxSum, arr[i - k], arr[i], currentSum);
     if (currentSum > maxSum) {
       maxSum = currentSum;
     }
-
   }
-  console.log(currentSum, maxSum);
-
   return maxSum;
 };
 // const maxSumOfNNumbers = (arr, k) => {
@@ -32,4 +30,5 @@ const maxSumOfNNumbers = (arr, k) => {
 //   return maxSum;
 // };
 // console.log(maxSumOfNNumbers([1, 2, 8, 9, 3, 20, 4, 5, 6, 7, 8, 9], 3));
-console.log(maxSumOfNNumbers([5,7, 6,3, 1, 20, 2,3,4,7, 6, 4], 3));
+console.log(maxSumOfNNumbers([5, 7, 6, 3, 1, 20, 2, 3, 4, 7, 6, 4], 3));
+console.log(maxSumOfNNumbers([1, 2, 8, 9, 3, 20, 4, 5, 6, 7, 8, 9], 3));

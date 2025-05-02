2c1ff080034913fb8b39855c116e48754825507e
Techie-Mahesh <maheshTech1205@gmail.com>
2025-05-02 19:07:51 +0530

    Reverse the string

---
+++ Strings/reverseTheString.js
@@ -0,0 +1,23 @@
+const reverseTheString = str => {
+  let reversedString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    let word = "";
+    if (str[i] === " ") {
+      continue;
+    }
+    while (str[i] !== " " && i >= 0) {
+      word = str[i] + word;
+      i--;
+    }
+    console.log(word);
+    if (word.length > 0) {
+      if (reversedString.length > 0) {
+        reversedString += " ";
+      }
+      reversedString = reversedString + word;
+    }
+  }
+  console.log(reversedString);
+};
+
+reverseTheString("  Hello    World  "); // Output: "olleH dlroW"
